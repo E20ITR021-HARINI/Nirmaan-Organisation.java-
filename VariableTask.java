4379ddf6bb0de65b44a100b7993eda7f082c3bda
E20ITR021-HARINI <148609742+E20ITR021-HARINI@users.noreply.github.com>
2024-12-27 16:00:24 +0530

    day2

---
+++ VariableTask.java
@@ -0,0 +1,33 @@
+public class VariableTask{
+
+  public static void main(String[] args){
+     int a=10;
+	System.out.println("int "+a);
+     byte x=-24;
+        System.out.println("byte " +x);
+     float y=2.67f;
+        System.out.println("float "+y);
+     double b=934.5027587d;
+        System.out.println("double "+b);
+    char c='c';
+     System.out.println("char "+c);
+   boolean d=true;
+ System.out.println("boolean "+d);
+ short e=30000;
+ System.out.println("short "+e);
+  long l=1234567652l;
+  System.out.println("long "+l);
+ String h="harini";
+  System.out.println("string "+h);
+  
+     
+
+}
+
+
+
+
+
+
+
+}
